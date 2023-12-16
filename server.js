5651ea13830d733652296a35d61d4568d0d0d756
ASHIR ANSARI <ansariashir777@gmail.com>
2023-12-16 21:26:02 +0530

    image fetch

--- server.js
+++ server.js
@@ -1,54 +1,50 @@
 const express = require("express");
 const path = require("path");
 const colors = require("colors");
-const moragan = require("morgan");
+const morgan = require("morgan");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
-var cors = require("cors");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 // dotenv
 dotenv.config();
 
-//mongodb connection
+// MongoDB connection
 connectDB();
-// rest object
+
+// Express app
 const app = express();
 
+// Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "public")));
+
+// Static file for images
+app.use("/userImages", express.static(path.join(__dirname, "userImages")));
+
+// Proxy middleware for images
 app.use(
   "/userImages",
+  (req, res, next) => {
+    console.log("Proxying request to target server:", req.url);
+    next();
+  },
   createProxyMiddleware({
     target: "https://mymuslimsaathi.com",
     changeOrigin: true,
   })
 );
 
-// middlewares
-app.use(cors());
-app.use(express.json());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(moragan("dev"));
-app.use(express.static(path.join(__dirname, "public")));
-
-// Static file for images
-app.use("/userImages", express.static("userImages"));
-app.use("/userImages", express.static(path.join(__dirname, "userImages")));
-app.use(
-  "/profile/userImages",
-  express.static(path.join(__dirname, "userImages"))
-);
-app.use(
-  "/admin-edit-user/:id/UserImages",
-  express.static(path.join(__dirname, "userImages"))
-);
-
-//admin
-app.use("/adsImages", express.static("adsImages"));
+// Admin
 app.use("/adsImages", express.static(path.join(__dirname, "adsImages")));
 
-// routes
+// Routes
 app.use("/api/user/", require("./routes/userRoutes"));
 app.use("/api/image/", require("./routes/imageUploadRoutes"));
 app.use("/api/contact/", require("./routes/contactRoutes"));
@@ -58,7 +54,7 @@ app.use("/api/admin/", require("./routes/adminRoutes"));
 // PORT
 const port = process.env.PORT || 8080;
 
-// STATIC FILES RUNNING ON BUILD FOLDER
+// Static files for production
 if (process.env.NODE_MODE === "production") {
   app.use(express.static(path.join(__dirname, "./client/build")));
   app.get("*", (req, res) => {
@@ -71,7 +67,7 @@ if (process.env.NODE_MODE === "production") {
 }
 
 // Listen
-app.listen(port, (req, res) => {
+app.listen(port, () => {
   console.log(
     `Server running in ${process.env.NODE_MODE} Mode on Port ${process.env.PORT}`
       .bgCyan
