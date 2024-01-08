19d3ad3a7713e54aa8676e9ec3b8e439a4dd0f5d
ASHIR ANSARI <ansariashir777@gmail.com>
2024-01-08 11:56:11 +0530

    fixes

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -199,7 +199,7 @@ router.post("/success", async (req, res) => {
     }, 1000);
     //end nodemailer
   }
-  res.redirect("https://halarishtey.com/user-dashboard");
+  res.redirect("https://halalrishtey.com/user-dashboard");
 });
 
 module.exports = router;
